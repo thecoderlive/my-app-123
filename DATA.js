39f09d45e8f8931ea4f3ca1b36d786a7c649bc25
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-17 14:50:50 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "abd8fb87-1568-4244-a126-6995805e908d",
+    "home_feed": [
+        {
+            "id": "ec62bdc7-2cee-427f-82d7-44c6a77584cf",
+            "artist_name": "same",
+            "home_carousel": [
+                {
+                    "id": "cc7e9b8b-e4c8-476f-b955-8e5bf9c685f2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png"
+                },
+                {
+                    "id": "c8aebfdd-f2eb-4cec-903c-610a3023df89",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/335.png"
+                },
+                {
+                    "id": "fd908da6-3ec4-48ff-aca5-4846bc61dfe8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/167.png"
+                },
+                {
+                    "id": "5e8071d2-26d4-4178-b73e-245cf040b3e1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                },
+                {
+                    "id": "29d0e8b1-899e-46fc-aca5-7149e161b480",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/205.png"
+                }
+            ],
+            "art_caption": "soldier",
+            "art_description": "Scene wall blue rise rise memory. Talk serve ground this wish."
+        },
+        {
+            "id": "c2307fb5-06a8-4b89-a94a-39b8dff4da11",
+            "artist_name": "paper",
+            "home_carousel": [
+                {
+                    "id": "99ca4817-c972-4d59-8872-72c07a0f0fb7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                },
+                {
+                    "id": "ec5b32d2-37dd-491e-8d0b-f71b92b3f4bd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/237.png"
+                },
+                {
+                    "id": "d1e8f54f-d2be-40c2-85c8-ca283ff755aa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                },
+                {
+                    "id": "91b00412-e043-4f40-acf6-1adcd2de4106",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/177.png"
+                },
+                {
+                    "id": "6c42da1b-3557-4c23-80e6-2bd249dad0d8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png"
+                }
+            ],
+            "art_caption": "role",
+            "art_description": "Add heavy find road oil look image. Southern worry evening participant current charge. Section know read support food consider team notice."
+        },
+        {
+            "id": "0b5b349b-2ca6-4222-9bd4-39801fe27f13",
+            "artist_name": "begin",
+            "home_carousel": [
+                {
+                    "id": "caf6615c-7a04-4bc3-b5e0-f68cb69138c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/171.png"
+                },
+                {
+                    "id": "73f5cf78-2a89-4762-a11e-1bef3c5b558b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/362.png"
+                },
+                {
+                    "id": "faef4865-1944-4628-ac5f-ac9a1f7dd932",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/92.png"
+                },
+                {
+                    "id": "162593da-87f1-454d-9be8-5a99c9cca8c4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png"
+                },
+                {
+                    "id": "c244fcc0-806c-4c72-b936-5c2f8ed3cc7c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                }
+            ],
+            "art_caption": "expect",
+            "art_description": "Market three hour ready wonder car any. However discussion eye successful."
+        },
+        {
+            "id": "091faeb9-d676-40aa-b263-7d1899e16f6c",
+            "artist_name": "thousand",
+            "home_carousel": [
+                {
+                    "id": "7de3bd6c-206f-4b87-9a92-16b98a89a3a9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png"
+                },
+                {
+                    "id": "6fa5477f-bee8-4076-a978-469b301cf77a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png"
+                },
+                {
+                    "id": "f85b88e5-b959-4cf7-a6a7-f2fe07df05d5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                },
+                {
+                    "id": "e87aca2d-3d2f-4618-8780-313018c2a95a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png"
+                },
+                {
+                    "id": "c43d4d70-2130-40a2-9997-3ecc8601c667",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/164.png"
+                }
+            ],
+            "art_caption": "action",
+            "art_description": "Past beautiful clear surface write throughout nothing. Work focus discussion box mother customer soldier look. Fight per all material place."
+        },
+        {
+            "id": "daf03e6d-e994-46dd-b03e-c5db711e295b",
+            "artist_name": "dog",
+            "home_carousel": [
+                {
+                    "id": "7a019eda-ecf4-493c-a933-402fbbbbaa23",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/318.png"
+                },
+                {
+                    "id": "e6010328-07f4-4f3d-84a3-cb9c9f8878ce",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/50.png"
+                },
+                {
+                    "id": "35567cd2-1eb6-4b34-8861-67007094ede5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/46.png"
+                },
+                {
+                    "id": "27597625-bd4c-49fe-bace-9d57c4d64a6a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/264.png"
+                },
+                {
+                    "id": "380e2710-c97f-4d75-9d38-fdd7b32d583a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png"
+                }
+            ],
+            "art_caption": "start",
+            "art_description": "Back government affect foreign seem. Attorney southern strategy own front item."
+        }
+    ]
+}
